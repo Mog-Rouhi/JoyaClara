37439d222a1c66461548fe35ece008d02da8ef18
Mog Rouhi <moghadase.roohi@gmail.com>
2022-12-11 21:27:50 +0000

    add first view

---
+++ JoyaClara/Controllers/JoyaController.cs
@@ -0,0 +1,23 @@
+﻿using JoyaClara.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoyaClara.Controllers
+{
+    public class JoyaController : Controller
+    {
+        private readonly IJoyaRepository _joyaRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public JoyaController(IJoyaRepository joyaRepository, ICategoryRepository categoryRepository)
+        {
+            _joyaRepository = joyaRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List() 
+        {
+            ViewBag.CurrentCategory = "Rings";
+            return View(_joyaRepository.AllJoyas);  
+        }
+    }
+}
